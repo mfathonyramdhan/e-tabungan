59b6307e7ad1dd3c37ff0a3dced8f0dd7003f419
Rieffff <129032228+Rieffff@users.noreply.github.com>
2023-09-19 19:42:48 +0700

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -118,6 +118,22 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-12 col-sm-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">Basic Bar Chart</h4>
+                            </div>
+                            <div class="card-body">
+                                <canvas id="barChart_1"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- <div class="row">
             <div class="col-lg-8">
                 <div class="card">
