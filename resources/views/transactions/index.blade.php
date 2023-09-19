2f1703284d27b38e6e4d56bfebc1d3d754bc37e6
Rieffff <129032228+Rieffff@users.noreply.github.com>
2023-09-19 20:12:13 +0700

    Update index.blade.php

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -10,12 +10,29 @@
             <div class="card">
                 <div class="card-header">
                     <h4 class="card-title">Histori Transaksi</h4>
-                    <a href="{{ route('transactions.print') }}" class="btn btn-primary" target="_blank">
-                        <i class="fa fa-print"></i> Print Semua HIstori Transaksi
-                    </a>
+                    
+                    <div class="bootstrap-modal">
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#basicModal"> <i class="fa fa-print"></i>Basic modal</button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="basicModal">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Modal title</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal">
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">Modal body text goes here.</div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-
                 <div class="card-body">
                     <div class="table-responsive">
                         <table id="example3" class="display" style="min-width: 845px">
@@ -92,6 +109,11 @@
                             </tfoot>
                         </table>
                     </div>
+                </div>
+                <div class="card-footer border-0 pt-0">
+                     <a href="{{ route('transactions.print') }}" class="btn btn-primary" target="_blank">
+                        <i class="fa fa-print"></i> Print Semua HIstori Transaksi
+                    </a>
                 </div>
             </div>
         </div>
